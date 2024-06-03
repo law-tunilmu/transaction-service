ad8f8b46748bc46a7bab24a6f0bbec03087d14ad
Raden Mohamad Adrian Ramadhan Hendar Wibawa <raden.mohamad11@ui.ac.id>
2024-06-03 20:13:25 +0700

    fix: fix singature verification function

--- app/utils.py
+++ app/utils.py
@@ -1,14 +1,22 @@
-import hmac
 import hashlib
 from app.config import MIDTRANS_SERVER_KEY
 # verify incoming notifications
 def verify_signature(notification_data):
 
-    signature = notification_data.get("signature")
-    order_id = notification_data.get("transaction_details", {}).get("order_id")
+    signature = notification_data.get("signature_key")
+    order_id = notification_data.get("order_id")
+    status_code = notification_data.get("status_code")
+    gross_amount = notification_data.get("gross_amount")
+
 
     # Create the data string to be signed
-    data = f"{order_id}{notification_data['transaction_status']}{notification_data['gross_amount']}{MIDTRANS_SERVER_KEY}"
-    hashed_data = hmac.new(MIDTRANS_SERVER_KEY.encode("utf-8"), data.encode("utf-8"), hashlib.sha512).hexdigest()
+    data = f"{order_id}{status_code}{gross_amount}{MIDTRANS_SERVER_KEY}"
 
-    return hashed_data == signature
+    sha512_hash = hashlib.sha512()
+
+    # Update the hash object with the string
+    sha512_hash.update(data.encode())
+
+    # Get the hexadecimal representation of the hash
+    hex_hash = sha512_hash.hexdigest()
+    return hex_hash == signature
