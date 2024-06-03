484322f201d27686cd9f15b44c32779c1b7596f9
Raden Mohamad Adrian Ramadhan Hendar Wibawa <raden.mohamad11@ui.ac.id>
2024-06-03 19:54:36 +0700

    fix: change notification data key

--- app/utils.py
+++ app/utils.py
@@ -8,7 +8,7 @@ def verify_signature(notification_data):
     order_id = notification_data.get("transaction_details", {}).get("order_id")
 
     # Create the data string to be signed
-    data = f"{order_id}{notification_data['status']}{notification_data['gross_amount']}{MIDTRANS_SERVER_KEY}"
+    data = f"{order_id}{notification_data['transaction_status']}{notification_data['gross_amount']}{MIDTRANS_SERVER_KEY}"
     hashed_data = hmac.new(MIDTRANS_SERVER_KEY.encode("utf-8"), data.encode("utf-8"), hashlib.sha512).hexdigest()
 
     return hashed_data == signature
